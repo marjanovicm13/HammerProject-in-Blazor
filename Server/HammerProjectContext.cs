ba724019fdce232f65039481d544f68aecd7d861
marjanovicm13 <marjanovicm314@gmail.com>
2023-03-06 02:30:56 +0100

    db context change

--- Server/HammerProjectContext.cs
+++ Server/HammerProjectContext.cs
@@ -19,28 +19,6 @@ namespace HammerProject.Server
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(450));
-            builder.Entity<IdentityRole>(entity => entity.Property(m => m.ConcurrencyStamp).HasColumnType("varchar(256)"));
-
-            builder.Entity<IdentityUserLogin<string>>(entity =>
-            {
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.ProviderKey).HasMaxLength(127);
-            });
-
-            builder.Entity<IdentityUserRole<string>>(entity =>
-            {
-                entity.Property(m => m.UserId).HasMaxLength(127);
-                entity.Property(m => m.RoleId).HasMaxLength(127);
-            });
-
-            builder.Entity<IdentityUserToken<string>>(entity =>
-            {
-                entity.Property(m => m.UserId).HasMaxLength(127);
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.Name).HasMaxLength(127);
-            });
         }
     }
 }
